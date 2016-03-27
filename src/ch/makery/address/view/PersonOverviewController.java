71f1790b5cdb6bd22bf20beae57c67dfa8cde91b
jardelkuhnen <jardelkuhnen@gmail.com>
2016-03-27 14:06:52 -0300

    Método showPersonDetails.

--- src/ch/makery/address/view/PersonOverviewController.java
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -1,11 +1,11 @@
 package ch.makery.address.view;
 
+import ch.makery.address.MainApp;
+import ch.makery.address.model.Person;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import ch.makery.address.MainApp;
-import ch.makery.address.model.Person;
 
 public class PersonOverviewController {
 	@FXML
@@ -46,6 +46,36 @@ public class PersonOverviewController {
 		// Inicializa a tablea de pessoa com duas colunas.
 		firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
 		lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+	}
+
+	/**
+	 * PReenche todos os campos de texto para mostrar detalhes sobre a pessoa.
+	 * Se a pessoa especificada for null, todos os campos de texto são limpos.
+	 * 
+	 * @param person
+	 *            a pessoa ou null
+	 */
+	private void showPersonDetails(Person person) {
+		if (person != null) {
+			// Preenche as labels com informações do objeto person.
+			firstNameLabel.setText(person.getFirstName());
+			lastNameLabel.setText(person.getLastName());
+			streetLabel.setText(person.getStreet());
+			postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+			cityLabel.setText(person.getCity());
+
+			// TODO: Nós precisamos de uma maneira de converter o aniversário em
+			// um String!
+			// birthdayLabel.setText(...);
+		} else {
+			// Person é null, remove todo o texto.
+			firstNameLabel.setText("");
+			lastNameLabel.setText("");
+			streetLabel.setText("");
+			postalCodeLabel.setText("");
+			cityLabel.setText("");
+			birthdayLabel.setText("");
+		}
 	}
 
 	/**
